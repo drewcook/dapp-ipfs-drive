e6e8be0d1aae887489346b30c5620ad337b52af7
Drew Cook <drewcook29@gmail.com>
2022-02-03 15:57:50 -0700

    chore: Redeploy to new Infura node

--- client/src/ipfsClient.js
+++ client/src/ipfsClient.js
@@ -1,5 +1,14 @@
 import { create } from 'ipfs-http-client'
 
+import crypto from 'crypto'
+
+const hash = crypto.createHmac(
+	'sha256',
+	'4ba4e5e0fc174cb4943e195f8b0623a9',
+	process.env.SHA_HASH_SECRET,
+)
+console.log(hash)
+
 const projectId = process.env.REACT_APP_INFURA_IPFS_PROJECT_ID
 const projectSecret = process.env.REACT_APP_INFURA_IPFS_PROJECT_SECRET
 const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
@@ -18,6 +27,7 @@ const infuraNode = {
 }
 
 const ipfsClientConfig = process.env.NODE_ENV === 'production' ? infuraNode : localNode
+console.log({ ipfsClientConfig })
 const ipfsClient = create(ipfsClientConfig)
 
 export default ipfsClient
