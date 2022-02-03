4811848adf234e3a1e21630b74b650fec1175fd0
Drew Cook <drewcook29@gmail.com>
2022-02-03 16:02:21 -0700

    chore: Log out env vars

--- client/src/ipfsClient.js
+++ client/src/ipfsClient.js
@@ -1,14 +1,5 @@
 import { create } from 'ipfs-http-client'
 
-import crypto from 'crypto'
-
-const hash = crypto.createHmac(
-	'sha256',
-	'4ba4e5e0fc174cb4943e195f8b0623a9',
-	process.env.SHA_HASH_SECRET,
-)
-console.log(hash)
-
 const projectId = process.env.REACT_APP_INFURA_IPFS_PROJECT_ID
 const projectSecret = process.env.REACT_APP_INFURA_IPFS_PROJECT_SECRET
 const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
@@ -27,7 +18,7 @@ const infuraNode = {
 }
 
 const ipfsClientConfig = process.env.NODE_ENV === 'production' ? infuraNode : localNode
-console.log({ ipfsClientConfig })
+console.log({ ipfsClientConfig, projectId, projectSecret }, process.env)
 const ipfsClient = create(ipfsClientConfig)
 
 export default ipfsClient
